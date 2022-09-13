92f8f1d7e62b5918e52a61b75a8eac4806fbccae
root <root@Angel.localdomain>
2022-09-13 00:23:13 -0500

    Changed navBar Work link to Projects link, Added Work content in Home page, added  the under construction and details in posts and projects

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,17 @@
 import Section from "../../components/section";
-
+import { Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 const Page = () => {
-  return (
-    <Section>
-      <h1>Posts page</h1>
-    </Section>
-  );
+	return (
+		<Section>
+			<h1>
+				Page is still under construction, however here you'll see posts
+				from{" "}
+				<Link href="https://www.instagram.com/magen.dev" isExternal>
+					@magen.dev <ExternalLinkIcon mx="2px" />
+				</Link>
+			</h1>
+		</Section>
+	);
 };
 export default Page;
---
+++ pages/projects/index.js
@@ -0,0 +1,13 @@
+import Section from "../../components/section";
+
+const Page = () => {
+	return (
+		<Section>
+			<h1>
+				Page is still under construction, however here you'll see
+				current projects and free time creations
+			</h1>
+		</Section>
+	);
+};
+export default Page;
